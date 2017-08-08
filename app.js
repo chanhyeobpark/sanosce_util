6a2b030bd61fd1dc8d74689ea26f2fcc3ba6404b
chan hyeob park <sanosce.park@gmail.com>
2017-08-08 02:49:02 +0000

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-
-
+var express        = require( 'express' );
+var http           = require( 'http' );
+var app            = express();
+app.set( 'port', process.env.PORT || 3001 );
 app.get('/', function (req, res) {
-   //res.send('Hello World!');
-   res.sendFile(__dirname + '/app/index.html');
+    res.send('Hello World');
 });
-
-
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
